883e457c8ecd885c5e4d2e869fc6ba0f838bf9a2
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-29 19:23:46 -0500

    Add: Tailwind css and prettier config

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -12,15 +12,20 @@ function PageLayout({
   children,
 }: PageLayoutProps) {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>{metaTitle}</title>
         <meta name="description" content={metaDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className="max-w-screen-md mx-auto text-center text-slate-400 py-8">
+        <h1 className="text-2xl md:text-4xl font-semibold inline-block">
+          My 2023 New&apos;s year&apos;s Resolutions
+        </h1>
+      </header>
       <main>{children}</main>
-    </div>
+    </>
   )
 }
 export default PageLayout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,63 @@
+import React, { useState } from 'react'
 import PageLayout from '../components/PageLayout'
-import styles from '../styles/Home.module.css'
+
+interface Resolution {
+  id: number
+  text: string
+}
 
 export default function Home() {
+  const [resolutions, setResolutions] = useState<Resolution[]>([])
+
+  const [newResolution, setNewResolution] = useState('')
+
+  const handleAddNewResolution = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (newResolution) {
+      setResolutions((prevResolutions) => [
+        ...prevResolutions,
+        { id: prevResolutions.length + 1, text: newResolution },
+      ])
+      setNewResolution('')
+    }
+  }
+
   return (
     <PageLayout metaTitle="2023 resolutions | Home">
-      <h1 className={styles.title}>Hola</h1>
+      <section className="max-w-screen-md text-slate-400 mx-auto">
+        <div className="flex flex-col gap-4 mx-16">
+          {resolutions.map((resolution) => (
+            <article
+              key={resolution.id}
+              className="flex flex-wrap justify-between items-center border border-slate-600 p-4 rounded-md cursor-pointer"
+            >
+              <div className="flex gap-4 items-center">
+                <div className="h-8 w-8 bg-slate-800 rounded-md"></div>
+                <h2 className="text-xl">{resolution.text}</h2>
+              </div>
+              <div className="flex flex-row gap-2">
+                <span className="flex-1">Edit</span>
+                <span className="flex-1">Delete</span>
+              </div>
+            </article>
+          ))}
+        </div>
+        <form
+          className="flex flex-row gap-2 mx-16"
+          onSubmit={handleAddNewResolution}
+        >
+          <input
+            className="flex-1 bg-slate-800 text-slate-100 rounded-md px-4 py-2 mt-4 outline-none"
+            type="text"
+            placeholder="Learn how to fly a plane"
+            value={newResolution}
+            onChange={(e) => setNewResolution(e.target.value)}
+          />
+          <button className="bg-slate-800 text-slate-100 rounded-md px-4 py-2 mt-4 hover:bg-slate-700">
+            Add new resolution
+          </button>
+        </form>
+      </section>
     </PageLayout>
   )
 }
